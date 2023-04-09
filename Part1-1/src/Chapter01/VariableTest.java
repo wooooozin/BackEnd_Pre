c6f80fa8b4644f3ecf17516b1cb7e7fccef837fd
wooooozin <wooooozin@gmail.com>
2023-04-10 06:52:41 +0900

    변수사용하기 1

--- Part1-1/src/Chapter01/VariableTest.java
+++ Part1-1/src/Chapter01/VariableTest.java
@@ -4,6 +4,25 @@ public class VariableTest {
 
     public static void main(String[] args) {
 
+        // 1. 변수 사용하기
+        System.out.println("** 변수 사용하기 **");
+        int age = 10;
+        System.out.println(age);
+
+        String country = "Korea";
+        System.out.println(country);
+
+        // 2. 변수 이름 규힉
+        // 문자, 숫자, _, % 사용가능
+        // 숫자로 시작 할 수 없다.
+        // 대소문자 구분
+        // 공백 사용할 수 없음
+        // 예약어 사용 안됨
+
+        // 3. 표기법
+        // 카멜 표기법 - 변수, 함수
+        // 파스칼 표기법 - 클래스
+
     }
 
 }
