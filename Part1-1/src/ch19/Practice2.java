f97b2616a1c367846c37a53da893703c8c784789
wooooozin <wooooozin@gmail.com>
2023-04-15 06:15:46 +0900

    연습문제

---
+++ Part1-1/src/ch19/Practice2.java
@@ -0,0 +1,31 @@
+package ch19;
+
+public class Practice2 {
+
+    public static String solution(int num) {
+
+        String result = "";
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        int i = 0;
+        while (num > 0) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result += roman[i];
+            }
+            i++;
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+        System.out.println(solution(6));
+        System.out.println(solution(1994));
+
+    }
+}
