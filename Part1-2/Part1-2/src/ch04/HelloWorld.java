25779bc68eeedf32e58ef5e41a9daf1a8df854cd
wooooozin <wooooozin@gmail.com>
2023-04-15 16:54:32 +0900

    자바 구조와 문장

---
+++ Part1-2/Part1-2/src/ch04/HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+package ch04; // 패키지 - 폴더, 마침표(.)으로 하위 패키지를 생성할 수 있 
+
+
+
+public class HelloWorld { 
+	
+	// 클래스 - 자바 프로그램 단위, 하나의 클래스는 하나의 소스 파일로 구성 (보통), public 키워드를 사용하는 경우는 파일 이름과 일치
+	
+	public static void main(String[] arsge) {
+		
+		System.out.println("hello");
+		
+	}
+	
+
+}
