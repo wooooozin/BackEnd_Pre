b7f518abf7eb8031119e53346fbb61ea52b12ff9
wooooozin <wooooozin@gmail.com>
2023-04-29 07:57:20 +0900

    연습문재

---
+++ Part2/src/ch02/ch12/Practice2.java
@@ -0,0 +1,12 @@
+package ch02.ch12;
+
+public class Practice2 {
+    public static void solution(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+
+    }
+}
